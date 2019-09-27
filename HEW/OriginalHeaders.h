96a35a8d6a8de40f7088690d08b9850d920777a5
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-09-27 21:39:45 +0900

    一括参照用ヘッダ作成

---
+++ HEW/OriginalHeaders.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//BaseClass
+#include "Component.h"
+#include "GameObject.h"
+#include "NonComponentGameObject.h"
+#include "Object.h"
+#include "Scene.h"
+#include "ScriptBase.h"
+#include "Singleton.h"
+#include "VisualComponent.h"
+
+//CommonClass
+#include "Color.h"
+#include "Texture.h"
+#include "Vector2.h"
+#include "Vector3.h"
+
+//Manager
+#include "Input.h"
+#include "LogWriter.h"
+#include "MyDirect3D.h"
+#include "ObjectManager.h"
+#include "SaveManager.h"
+#include "SceneManager.h"
+#include "Setting.h"
+#include "Time.h"
+
+//Components
+#include "Sprite.h"
+
+//Scripts
