00a765ade2007c5f1e03672f2661b7a2625084b7
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-10-01 20:17:14 +0900

    NonComponentGameObjectを削除、GameObjectに統一

--- HEW/GameObject.cpp
+++ HEW/GameObject.cpp
@@ -1,15 +1,134 @@
 #include "GameObject.h"
-
-
+#include "LogWriter.h"
 
 GameObject::GameObject() {
+	position = new Vector3(0,0,0);
+	localPosition = new Vector3(0,0,0);
+	scale = new Vector3(1, 1, 1);
+	localScale = new Vector3(1, 1, 1);
+	rotation = new Vector3(0, 0, 0);
+	velocity = new Vector3(0,0,0);
+	LogWriter::GetInstance().Log("生成されました\n");
 }
-
 
 GameObject::~GameObject() {
+	int childSize = child.size();
+	LogWriter::GetInstance().Log("子供の数：%d\n", childSize);
+	for (int i = 0; i < childSize; i++) {
+		delete child[i];
+	}
+	delete position;
+	delete localPosition;
+	delete scale;
+	delete localScale;
+	delete rotation;
+	delete velocity;
+	LogWriter::GetInstance().Log("ゲームオブジェクトを削除しました\n");
 }
 
-Component * GameObject::GetComponent(unsigned int num) {
+Vector3* GameObject::GetPosition() {
+	return position;
+}
+
+void GameObject::SetPosition(Vector3* newPos) {
+	delete position;
+	position = newPos;
+}
+
+Vector3* GameObject::GetLocalPosition() {
+	return localPosition;
+}
+
+void GameObject::SetLocalPosition(Vector3* newPos) {
+	localPosition = newPos;
+}
+
+Vector3* GameObject::GetScale() {
+	return scale;
+}
+
+void GameObject::SetScale(Vector3* newScale) {
+	scale = newScale;
+}
+
+Vector3* GameObject::GetRotation() {
+	return rotation;
+}
+
+void GameObject::SetRotation(Vector3* newRotation) {
+	delete rotation;
+	rotation = newRotation;
+}
+
+Vector3* GameObject::GetLocalScale() {
+	return localScale;
+}
+
+void GameObject::SetLocalScale(Vector3* newScale) {
+	delete localScale;
+	localScale = newScale;
+}
+
+void GameObject::SetVelocity(Vector3* newVelocity) {
+	velocity = newVelocity;
+}
+
+Vector3* GameObject::GetVelocity() {
+	return velocity;
+}
+
+string GameObject::GetName() {
+	return name;
+}
+
+void GameObject::SetName(string newName) {
+	name = newName;
+}
+
+string GameObject::GetTag() {
+	return tag;
+}
+
+void GameObject::SetTag(string newTag) {
+	tag = newTag;
+}
+
+bool GameObject::CompareTag(string target) {
+	return tag == target;
+}
+
+GameObject * GameObject::GetParent() {
+	return nullptr;
+}
+
+void GameObject::SetParent(GameObject * parent) {
+}
+
+GameObject * GameObject::GetChild(int index) {
+	return nullptr;
+}
+
+int GameObject::GetChildCount() {
+	return child.size();
+}
+
+void GameObject::AddChild(GameObject * child) {
+}
+
+void GameObject::Rotate(Vector3 angle) {
+	for (int i = 0; i < GetChildCount(); i++) {
+		GetChild(i)->Rotate(angle * -1);
+	}
+}
+
+void GameObject::Move(Vector3 range) {
+	for (int i = 0; i < GetChildCount(); i++) {
+		GetChild(i)->Move(range);
+	}
+	SetPosition(new Vector3(position->GetX() + range.GetX(), position->GetY() + range.GetY(), position->GetZ()+ range.GetZ()));
+}
+
+Component* GameObject::GetComponent(unsigned int num) {
 	if (num >= components.size() || num < 0) {
 		LogWriter::GetInstance().LogWorning("コンポーネントの取得に失敗しました。\nゲームオブジェクト名：%s\nコンポーネント引数：%d", name, num);
 		return nullptr;
@@ -22,7 +141,7 @@ unsigned int GameObject::GetComponentCount() {
 }
 
 
-void GameObject::AddComponent(Component * component) {
+void GameObject::AddComponent(Component* component) {
 	components.emplace_back(component);
+	component->SetGameObject(this);
 }
-
--- HEW/GameObject.h
+++ HEW/GameObject.h
@@ -1,14 +1,72 @@
 #pragma once
-#include "NonComponentGameObject.h"
-#include "Component.h"
-#include "LogWriter.h"
+using namespace std;
+#include "Object.h"
+#include "Vector3.h"
 #include <vector>
-#include <typeinfo.h>
+#include <string>
+#include "Component.h"
 
-class GameObject : public NonComponentGameObject {
+class GameObject : public Object {
 public:
-	GameObject();
-	~GameObject();
+
+	//オブジェクトの座標を取得します
+	Vector3* GetPosition();
+	//オブジェクトの座標を設定します
+	void SetPosition(Vector3* newPos);
+
+	//親との相対座標を取得します
+	Vector3* GetLocalPosition();
+	//親との相対座標を設定します
+	void SetLocalPosition(Vector3* newPos);
+
+	//現在の大きさを取得します
+	Vector3* GetScale();
+	//大きさを設定します
+	void SetScale(Vector3* newScale);
+
+	//現在の回転を取得します
+	Vector3* GetRotation();
+	//回転角度を設定します
+	void SetRotation(Vector3* newRotation);
+
+	//このオブジェクト自体の拡大率を取得します
+	Vector3* GetLocalScale();
+	//このオブジェクト自体の拡大率を設定します
+	void SetLocalScale(Vector3* newScale);
+
+	//現在の速度を取得します
+	Vector3* GetVelocity();
+	//速度を設定します
+	void SetVelocity(Vector3* newVelocity);
+
+	//このオブジェクトの名前を取得します
+	string GetName();
+	//このオブジェクトの名前を設定します
+	void SetName(string newName);
+
+	//このオブジェクトのタグを取得します
+	string GetTag();
+	//このオブジェクトにタグを設定します
+	void SetTag(string newTag);
+	//タグとtargetが一致しているかを返します
+	bool CompareTag(string target);
+
+	//親のオブジェクトを取得します
+	GameObject* GetParent();
+	//親を設定します
+	void SetParent(GameObject* parent);
+
+	//index番目の子供オブジェクトを取得します
+	GameObject* GetChild(int index);
+	//子供オブジェクトの数を取得します
+	int GetChildCount();
+	//子供を追加します
+	void AddChild(GameObject* child);
+
+	//オブジェクトを回転させます
+	void Rotate(Vector3 angle);
+
+	void Move(Vector3 range);
 
 	template<typename T>
 	Component* GetComponent(T* t) {
@@ -57,10 +115,28 @@ public:
 			//引数がコンポーネントじゃなかった
 			return;
 		}
-	
+
 	}
 
-protected:
-	vector<Component*> components;
+	GameObject();
+	~GameObject();
 
+protected:
+	bool isActive;
+
+	Vector3* position;
+	Vector3* localPosition;
+	Vector3* scale;
+	Vector3* localScale;
+	Vector3* rotation;
+	Vector3* velocity;
+
+	string name;
+	string tag;
+
+	GameObject* parent;
+	vector<GameObject*> child{};
+
+	vector<Component*> components;
 };
+
