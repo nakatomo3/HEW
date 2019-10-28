3dea2834ef845a598bfa51b91ce8c83a98ebce68
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-10-28 16:17:45 +0900

    ゲージのスプライトを再利用するように

--- HEW/Run.cpp
+++ HEW/Run.cpp
@@ -14,7 +14,6 @@ void Run::Start() {
 		ObjectManager::GetInstance().Instantiate(playerObjects[i]);
 		ObjectManager::GetInstance().Instantiate(gaugeObjects[i]);
 	}
-	//LogWriter::GetInstance().Log("hoge");
 }
 
 void Run::Load() {
@@ -36,38 +35,33 @@ void Run::Load() {
 
 	//このコメントの下にゲージ関連の処理を追加
 
-	//左上仮ゲージ
-	Sprite* gaugeSprites = new Sprite();
-	gaugeSprites->SetScale(new Vector2(25, 200));//横,縦 の大きさ
-	GameObject* gaugeObjects = new GameObject();
-	gaugeObjects->SetPosition(new Vector3(50, 150, 0));//x,y,z座標
-	gaugeObjects->AddComponent(gaugeSprites);
-	ObjectManager::GetInstance().Instantiate(gaugeObjects);
+	if (playerCount >= 1) {
+		//左上仮ゲージ
+		gaugeSprites[0]->SetScale(new Vector2(25, 200));//横,縦 の大きさ
+		gaugeObjects[0]->SetPosition(new Vector3(50, 150, 0));//x,y,z座標
+		gaugeObjects[0]->AddComponent(gaugeSprites[0]);
+	}
 
-	//右上仮ゲージ
-	Sprite* gaugeSprites2 = new Sprite();
-	gaugeSprites2->SetScale(new Vector2(25, 200));//横,縦 の大きさ
-	GameObject* gaugeObjects2 = new GameObject();
-	gaugeObjects2->SetPosition(new Vector3(550, 150, 0));//x,y,z座標
-	gaugeObjects2->AddComponent(gaugeSprites2);
-	ObjectManager::GetInstance().Instantiate(gaugeObjects2);
+	if (playerCount >= 2) {
+		//右上仮ゲージ
+		gaugeSprites[1]->SetScale(new Vector2(25, 200));//横,縦 の大きさ
+		gaugeObjects[1]->SetPosition(new Vector3(550, 150, 0));//x,y,z座標
+		gaugeObjects[1]->AddComponent(gaugeSprites[1]);
+	}
 
-	//左下仮ゲージ
-	Sprite* gaugeSprites3 = new Sprite();
-	gaugeSprites3->SetScale(new Vector2(25, 200));//横,縦 の大きさ
-	GameObject* gaugeObjects3 = new GameObject();
-	gaugeObjects3->SetPosition(new Vector3(50, 450, 0));//x,y,z座標
-	gaugeObjects3->AddComponent(gaugeSprites3);
-	ObjectManager::GetInstance().Instantiate(gaugeObjects3);
+	if (playerCount >= 3) {
+		//左下仮ゲージ
+		gaugeSprites[2]->SetScale(new Vector2(25, 200));//横,縦 の大きさ
+		gaugeObjects[2]->SetPosition(new Vector3(50, 450, 0));//x,y,z座標
+		gaugeObjects[2]->AddComponent(gaugeSprites[2]);
+	}
 
-	//右下仮ゲージ
-	Sprite* gaugeSprites4 = new Sprite();
-	gaugeSprites4->SetScale(new Vector2(25, 200));//横,縦 の大きさ
-	GameObject* gaugeObjects4 = new GameObject();
-	gaugeObjects4->SetPosition(new Vector3(550, 450, 0));//x,y,z座標
-	gaugeObjects4->AddComponent(gaugeSprites4);
-	ObjectManager::GetInstance().Instantiate(gaugeObjects4);
-
+	if (playerCount >= 4) {
+		//右下仮ゲージ
+		gaugeSprites[3]->SetScale(new Vector2(25, 200));//横,縦 の大きさ
+		gaugeObjects[3]->SetPosition(new Vector3(550, 450, 0));//x,y,z座標
+		gaugeObjects[3]->AddComponent(gaugeSprites[3]);
+	}
 
 	//このコメントの下にプレイヤー関連の処理を追加
 
