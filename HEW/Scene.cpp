be5c14f2140e6c827b7bc9b64f26fcf7bf777752
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-11-08 12:21:41 +0900

    シーンに変数管理機能を追加

--- HEW/Scene.cpp
+++ HEW/Scene.cpp
@@ -28,3 +28,35 @@ void Scene::Update() {
 void Scene::Destroy() {
 	delete this;
 }
+
+bool Scene::GetBool(string name) {
+	return boolMap[name];
+}
+
+int Scene::GetInt(string name) {
+	return intMap[name];
+}
+
+float Scene::GetFloat(string name) {
+	return floatMap[name];
+}
+
+string Scene::GetString(string name) {
+	return stringMap[name];
+}
+
+void Scene::SetBool(string name, bool value) {
+	boolMap[name] = value;
+}
+
+void Scene::SetInt(string name, int value) {
+	intMap[name] = value;
+}
+
+void Scene::SetFloat(string name, float value) {
+	floatMap[name] = value;
+}
+
+void Scene::SetString(string name, string value) {
+	stringMap[name] = value;
+}
--- HEW/Scene.h
+++ HEW/Scene.h
@@ -21,9 +21,24 @@ public:
 
 	void Destroy();
 
+	bool GetBool(string name);
+	int GetInt(string name);
+	float GetFloat(string name);
+	string GetString(string name);
+
+	void SetBool(string name, bool value);
+	void SetInt(string name, int value);
+	void SetFloat(string name, float value);
+	void SetString(string name, string value);
+
 protected:
 
 	~Scene();
+
+	map<string, bool> boolMap;
+	map<string, int> intMap;
+	map<string, float> floatMap;
+	map<string, string> stringMap;
 
 private:
 
