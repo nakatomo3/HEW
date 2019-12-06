7a2706b3ab9cad6348aa8453aa4e829287a33be9
yanagyi <yanahiro429@gmail.com>
2019-12-06 18:10:33 +0900

    毎フレームログを削除(中村)

--- HEW/PlayerRun.cpp
+++ HEW/PlayerRun.cpp
@@ -43,29 +43,21 @@ void PlayerRun::Update() {
 	case 0:
 		if (Input::GetInstance().GetKey(DIK_A) && run->timer >= 4 && run->timer <= 10) { // 左上ゲージ 小さい振ってが表示されると同時にAキーでチャージ可能
 			charge += 10.0f; // 値は調整                                                                         よーいが表示されるとチャージは不可能
-			LogWriter::GetInstance().Log("A"); // 押されているかの確認ログ 表示：A
-			LogWriter::GetInstance().Log("%d:%f", playerID, charge); // チャージされているかのログ
 		}
 		break;
 	case 1:
 		if (Input::GetInstance().GetKey(DIK_S) && run->timer >= 4 && run->timer <= 10) { // 右上ゲージ 小さい振ってが表示されると同時にSキーでチャージ可能
 			charge += 10.0f; // 値は調整                                                                         よーいが表示されるとチャージは不可能
-			LogWriter::GetInstance().Log("S"); // 押されているかの確認ログ 表示：S
-			LogWriter::GetInstance().Log("%d:%f", playerID, charge); // チャージされているかのログ
 		}
 		break;
 	case 2:
 		if (Input::GetInstance().GetKey(DIK_D) && run->timer >= 4 && run->timer <= 10) { // 左下ゲージ 小さい振ってが表示されると同時にDキーでチャージ可能
 			charge += 10.0f; // 値は調整                                                                         よーいが表示されるとチャージは不可能
-			LogWriter::GetInstance().Log("D"); // 押されているかの確認ログ 表示：D
-			LogWriter::GetInstance().Log("%d:%f", playerID, charge); // チャージされているかのログ
 		}
 		break;
 	case 3:
 		if (Input::GetInstance().GetKey(DIK_F) && run->timer >= 4 && run->timer <= 10) { // 右下ゲージ 小さい振ってが表示されると同時にFキーでチャージ可能
 			charge += 10.0f; // 値は調整                                                                         よーいが表示されるとチャージは不可能
-			LogWriter::GetInstance().Log("F"); // 押されているかの確認ログ 表示：F
-			LogWriter::GetInstance().Log("%d:%f", playerID, charge); // チャージされているかのログ
 		}
 		break;
 	}
@@ -80,7 +72,7 @@ void PlayerRun::Update() {
 
 	//角度8度ぐらいを0.1秒で移動
 	if (run->timer >= 12 && isReplay == false) {
-		gameObject->Move(Vector3(charge * 0.05f * SCREEN_WIDTH/1024, charge * -0.00f  * SCREEN_HEIGHT/576, 0));
+		gameObject->Move(Vector3(charge * 0.03f * SCREEN_WIDTH/1024, charge * -0.00f  * SCREEN_HEIGHT/576, 0));
 		run->SetTime(playerID, 500 / charge);
 	}
 
@@ -91,9 +83,11 @@ void PlayerRun::Update() {
 
 	//リプレイ速度の移動
 	if (isReplay == true) {
-		LogWriter::GetInstance().Log("charge:%f", charge);
-		LogWriter::GetInstance().Log("all:%f", charge * 0.005 * SCREEN_WIDTH / 1024.0);
-		gameObject->Move(Vector3(charge * 0.005 * SCREEN_WIDTH / 1024.0, charge * -0.00f  * SCREEN_HEIGHT / 576, 0));
+		if (run->timer <= 22.5f) {
+			gameObject->Move(Vector3(charge * 0.003f * SCREEN_WIDTH / 1024.0, charge * -0.00f  * SCREEN_HEIGHT / 576, 0));
+		} else {
+			gameObject->Move(Vector3(charge * 0.002f * SCREEN_WIDTH / 1024.0, charge * -0.00f  * SCREEN_HEIGHT / 576, 0));
+		}
 	}
 }
 
