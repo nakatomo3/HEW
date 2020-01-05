3406463547080fbcde07a4aa0f03dfe7b2357eb4
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2020-01-05 11:52:19 +0900

    人数セレクトシーンを追加

---
+++ HEW/SelectPlayerCount.cpp
@@ -0,0 +1,20 @@
+#include "SelectPlayerCount.h"
+
+SelectPlayerCount::SelectPlayerCount(string name) : Scene(name) {
+
+}
+
+SelectPlayerCount::~SelectPlayerCount() {
+}
+
+void SelectPlayerCount::Load() {
+}
+
+void SelectPlayerCount::UnLoad() {
+}
+
+void SelectPlayerCount::Start() {
+}
+
+void SelectPlayerCount::Update() {
+}
