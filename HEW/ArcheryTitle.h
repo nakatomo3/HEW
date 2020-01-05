2ac9b0fb352b1931c0ae906db3684600fd4b5b16
toukaiza41 <yufumami@ezweb.ne.jp>
2020-01-05 13:28:44 +0900

    タイトル(アーチェリー)の作成、背景の表示とタイトルロゴの表示、 タイトルアニメの作成(画像の大きさ変更の必要あり)

--- HEW/ArcheryTitle.h
+++ HEW/ArcheryTitle.h
@@ -21,6 +21,8 @@ public:
 
 private:
 
+	bool isManual = false;
+
 	GameObject* backgroundObject;
 	Sprite* backgroundSprite;
 
@@ -33,7 +35,28 @@ private:
 	Texture* okTexture;
 	vector<Texture*> playerTexture;
 
+	GameObject* archeryObject;
+	Sprite* archerySprite;
+
+	Sprite* archeryLogo1;
+	Sprite* archeryLogo2;
+	Sprite* archeryLogo3;
+	Sprite* archeryLogo4;
+	Sprite* archeryLogo5;
+	Sprite* archeryLogo6;
+
+	Texture* archeryTexture1;
+	Texture* archeryTexture2;
+	Texture* archeryTexture3;
+	Texture* archeryTexture4;
+	Texture* archeryTexture5;
+	Texture* archeryTexture6;
+
+	int logoSpeed = 4000;
+
 	int playerCount = 0;
+
+	float timer = 0;
 
 	vector<bool> isReady;
 	
