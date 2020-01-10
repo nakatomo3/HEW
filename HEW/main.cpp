59e6ed151deb1dcd2c40196f7a51114682e44a79
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2020-01-10 17:04:39 +0900

    最初に読み込むシーンをタイトルに

--- HEW/main.cpp
+++ HEW/main.cpp
@@ -169,7 +169,7 @@ bool Init(){
 	Scene* archeryTitle = new ArcheryTitle("archeryTitle");
 	Scene* archeryResult = new ArcheryResult("ArcheryResult");
 
-	SceneManager::GetInstance().LoadScene("run");//仮のScene遷移機能
+	SceneManager::GetInstance().LoadScene("Title");//仮のScene遷移機能
 	
 
 	//例。こんな感じで初期化する。シーンにまとめるのを推奨
