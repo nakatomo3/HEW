1149d9f08cd9758acc614f3eaec5ea70d918f18c
toukaiza41 <yufumami@ezweb.ne.jp>
2020-01-24 12:46:23 +0900

    総合結果発表のタイトルアニメの作成とタイトルロゴの表示と総合結果発表の画像追加

--- HEW/TotalResult.h
+++ HEW/TotalResult.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Scene.h"
+#include "Sprite.h"
+#include "Texture.h"
 class TotalResult : public Scene {
 public:
 	TotalResult(string name);
@@ -8,8 +10,33 @@ public:
 	void Load();
 	void Start();
 	void UnLoad();
+	void Update();
 
 private:
+
+	GameObject* titleObject;
+	Sprite* titleSprite;
+
+	GameObject* backgroundObject;
+	Sprite* backgroundSprite;
+
+	Sprite* totalresultLogo;
+	Sprite* totalresultLogo1;
+	Sprite* totalresultLogo2;
+	Sprite* totalresultLogo3;
+	Sprite* totalresultLogo4;
+	Sprite* totalresultLogo5;
+	Sprite* totalresultLogo6;
+	
+
+	Texture* logoTexture1;
+	Texture* logoTexture2;
+	Texture* logoTexture3;
+	Texture* logoTexture4;
+	Texture* logoTexture5;
+	Texture* logoTexture6;
+
+	float timer = 0;
 
 };
 
